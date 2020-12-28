613557a21976cc81c308986c21816b9a9374b4b1
CindoddCindy <cindodcindy@gmail.com>
2020-12-28 15:27:09 +0700

    many to many test folder add

---
+++ manytomanytest/src/main/java/manytomany/manytomanytest/ManytomanytestApplication.java
@@ -0,0 +1,13 @@
+package manytomany.manytomanytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytomanytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytomanytestApplication.class, args);
+	}
+
+}
---
+++ manytomanytest/src/test/java/manytomany/manytomanytest/ManytomanytestApplicationTests.java
@@ -0,0 +1,13 @@
+package manytomany.manytomanytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytomanytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
